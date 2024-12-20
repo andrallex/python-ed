ae77cd383e132986b9091dbd5e48d2ed6f381ff0
demiurge <andrallex@outlook.com>
2024-12-20 12:50:04 +0400

    natasha_test.py is added

---
+++ home_tasks/natasha_test.py
@@ -0,0 +1,30 @@
+from natasha import (
+    Segmenter,
+    
+    NewsEmbedding,
+    NewsMorphTagger,
+    NewsSyntaxParser,
+    
+    Doc
+)
+
+text = 'Специалисты собрали более трех тысяч тонн загрязненного песка с кубанского берега после крушения танкеров с нефтепродуктами, сообщили в пресс-службе МЧС.'
+
+segmenter = Segmenter()
+
+emb = NewsEmbedding()
+morph_tagger = NewsMorphTagger(emb)
+syntax_parser = NewsSyntaxParser(emb)
+
+doc = Doc(text)
+
+doc.segment(segmenter)
+doc.tag_morph(morph_tagger)
+doc.parse_syntax(syntax_parser)
+
+sent = doc.sents[0]
+sent.morph.print()
+    
+
+sent.syntax.print()
+        
